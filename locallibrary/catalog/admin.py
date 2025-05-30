fa7e6a294767fa2b5677d98f74290d0cc1dc4804
naphtron <naphtron@yahoo.com>
2025-05-30 16:00:20 +0300

    registered models

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
+# Register the models with the admin site
+# The models are registered with the admin site to make them manageable through the Django admin interface.
