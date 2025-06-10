9e3c323c0864effcc46d72604713d6903d3d4d6a
naphtron <naphtron@yahoo.com>
2025-06-10 16:16:59 +0300

    generic list and detailed views

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # Genres (count distinct genres)
+    num_genres = Genre.objects.count()
+
+    #Books that contain a particular word in the title
+    word_in_title = Book.objects.filter(title__icontains='Harry').values_list('title', flat=True)
+    title_string = ', '.join(word_in_title)
+    word_in_title = f"Books containing 'Harry' in the title: {title_string}" if title_string else "No books found with 'Harry' in the title."
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'word_in_title': word_in_title
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
